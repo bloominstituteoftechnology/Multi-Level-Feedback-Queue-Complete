7a98a7fcaef80b498bac26608606d0f330d29ac4
Sean Chen <skypemaster007@gmail.com>
2018-06-29 08:43:14 -0500

    Add more clarifying comments

--- src/Process.js
+++ src/Process.js
@@ -1,11 +1,17 @@
 const { SchedulerInterrupt } = require('./constants/index');
 
+// A class representation of a process that may be blocking
+// or non-blocking. We can specify how much CPU time a process
+// needs in order to complete, or we can specify if the process
+// is blocking; if so, the amount of blocking time needed is
+// randomly determined.
 class Process {
     constructor(pid, cpuTimeNeeded=null, blocking=false) {
         this._pid = pid;
         this.queue = null;
         this.cpuTimeNeeded = (cpuTimeNeeded !== null) ? cpuTimeNeeded : Math.round(Math.random() * 1000);
         this.blockingTimeNeeded = blocking ? Math.round(Math.random() * 100) : 0;
+        // A bool representing whether this process was toggled from blocking to non-blocking or vice versa
         this.stateChanged = false;
     }
     
@@ -17,27 +23,40 @@ class Process {
         return (this.cpuTimeNeeded === 0 && this.blockingTimeNeeded === 0);
     }
 
+    // If no blocking time is needed by this process, decrement the amount of 
+    // CPU time it needs by the input time
+    // If blocking time is needed by this process, move it to the blocking queue
+    // by emitting the appropriate interrupt
+    // Make sure the `stateChanged` flag is toggled appropriately
     executeProcess(time) {
         this.stateChanged = false;
         if (this.blockingTimeNeeded === 0) {
             this.cpuTimeNeeded -= time;
+            // this line ensures that cpuTimeNeeded never gets set below 0
             this.cpuTimeNeeded = this.cpuTimeNeeded > 0 ? this.cpuTimeNeeded : 0;
         } else {
+            // emit the PROCESS_BLOCKED interrupt
             this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_BLOCKED);
-            this.stateChanged = true; 
+            this.stateChanged = true;
         }
    }
 
+   // If this process requires blocking time, decrement the amount of blocking
+   // time it needs by the input time
+   // Once it no longer needs to perform any blocking execution, move it to the 
+   // top running queue by emitting the appropriate interrupt
+   // Make sure the `stateChanged` flag is toggled appropriately
     executeBlockingProcess(time) {
         this.blockingTimeNeeded -= time;
         this.blockingTimeNeeded = this.blockingTimeNeeded > 0 ? this.blockingTimeNeeded : 0;
-        
+
         if (this.blockingTimeNeeded === 0) {
             this.queue.emitInterrupt(this, SchedulerInterrupt.PROCESS_READY);
             this.stateChanged = true;
         }
     }
 
+    // Returns this process's stateChanged property
     isStateChanged() {
         return this.stateChanged;
     }
@@ -46,10 +65,10 @@ class Process {
         return this._pid;
     }
 
+    // Private function used for testing; DO NOT MODIFY
     _getParentQueue() {
         return this.queue;
     }
 }
 
 module.exports = Process;
-
--- src/Scheduler.js
+++ src/Scheduler.js
@@ -2,29 +2,44 @@ const Queue = require('./Queue');
 const { 
     QueueType,
     PRIORITY_LEVELS,
+    SchedulerInterrupt,
 } = require('./constants/index');
 
+// A class representing the scheduler
+// It holds a single blocking queue for blocking processes and three running queues 
+// for non-blocking processes
 class Scheduler { 
     constructor() { 
         this.clock = Date.now();
         this.blockingQueue = new Queue(this, 50, 0, QueueType.BLOCKING_QUEUE);
         this.runningQueues = [];
-
+        // Initialize all the CPU running queues
         for (let i = 0; i < PRIORITY_LEVELS; i++) {
             this.runningQueues[i] = new Queue(this, 10 + i * 20, i, QueueType.CPU_QUEUE);
         }
     }
 
+    // Executes the scheduler in an infinite loop as long as there are processes in any of the queues
+    // Calculate the time slice for the next iteration of the scheduler by subtracting the current
+    // time from the clock property. Don't forget to update the clock property afterwards.
+    // On every iteration of the scheduler, if the blocking queue is not empty, blocking work
+    // should be done. Once the blocking work has been done, perform some CPU work in the same iteration.
     run() {
         while (true) {
+            // log the current time 
             const time = Date.now();
+            // take the difference between the current time and the 
+            // time logged by the last loop iteration
             const workTime = time - this.clock;
+            // update this.clock
             this.clock = time;
 
+            // check the block queue to see if there are any processes there
             if (!this.blockingQueue.isEmpty()) {
                 this.blockingQueue.doBlockingWork(workTime);
             }
 
+            // do some work on the running queues
             for (let i = 0; i < PRIORITY_LEVELS; i++) {
                 const queue = this.runningQueues[i];
                 if (!queue.isEmpty()) {
@@ -33,43 +48,39 @@ class Scheduler {
                 }
             }
 
+            // check if all the queues are empty
             if (this.allQueuesEmpty()) {
-                console.log("No more processes! I can sleep now.");
+                console.log("Idle mode");
                 break;
             }
         }
     }
 
     allQueuesEmpty() {
-        return this.runningQueues.every(queue => queue.isEmpty()) && this.blockingQueue.isEmpty();
+        return this.runningQueues.every((queue) => queue.isEmpty()) && this.blockingQueue.isEmpty();
     }
 
     addNewProcess(process) {
+        // new processes will only ever get added to the top-level running queue
         this.runningQueues[0].enqueue(process);
     }
 
+    // The scheduler's interrupt handler that receives a queue, a process, and an interrupt string constant
+    // Should handle PROCESS_BLOCKED, PROCESS_READY, and LOWER_PRIORITY interrupts.
     handleInterrupt(queue, process, interrupt) {
         switch(interrupt) {
-            case 'PROCESS_BLOCKED':
+            case SchedulerInterrupt.PROCESS_BLOCKED:
                 this.blockingQueue.enqueue(process);
                 break;
-            case 'PROCESS_READY':
+            case SchedulerInterrupt.PROCESS_READY:
                 this.addNewProcess(process);
                 break;
-            case 'LOWER_PRIORITY':
-                /* More explicit way to write this logic */
-                // if (queue.getQueueType() === QueueType.BLOCKING_QUEUE) {
-                //     queue.enqueue(process);
-                //     break;
-                // }
-                // const priority = queue.getPriorityLevel();
-                // if (priority === PRIORITY_LEVELS - 1) {
-                //     queue.enqueue(process);
-                //     break;
-                // }
-                // this.runningQueues[priority + 1].enqueue(process);
-                // break;
+            case SchedulerInterrupt.LOWER_PRIORITY:
                 if (queue.getQueueType() === QueueType.CPU_QUEUE) {
+                    // move to the next priority queue
+                    // figure out the priority level of the next queue
+                    // remember to handle the case where the process is already in the lowest 
+                    // priority queue
                     const priorityLevel = Math.min(PRIORITY_LEVELS - 1, queue.getPriorityLevel() + 1);
                     this.runningQueues[priorityLevel].enqueue(process);
                 } else {
@@ -81,14 +92,15 @@ class Scheduler {
         }
     }
 
+    // Private function used for testing; DO NOT MODIFY
     _getCPUQueue(priorityLevel) {
         return this.runningQueues[priorityLevel];
     }
 
+    // Private function used for testing; DO NOT MODIFY
     _getBlockingQueue() {
         return this.blockingQueue;
     }
 }
 
 module.exports = Scheduler;
-
